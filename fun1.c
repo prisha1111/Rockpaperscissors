ba2821940b0fb252c5cfe48732efc3052e9eb382
Revabindra <122342037+Revabindra@users.noreply.github.com>
2023-03-23 09:40:27 +0530

    Add files via upload

---
+++ fun1.c
@@ -0,0 +1,14 @@
+void printhello();
+void printgoodbye();
+
+int main(){
+	printhello();
+	printgoodbye();
+	return 0;
+}
+void printgoodbye(){
+	printf("\ngoodbye");
+}
+void printhello(){
+	printf("Hello");
+}
